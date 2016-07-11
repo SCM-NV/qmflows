3566ca0ca12e7b75f3755ee4784307751a9e0d94
Lars Ridder <l.ridder@esciencecenter.nl>
2016-07-11 14:43:02 +0200

    Enable replacement of all keyword value pairs of a specific Settings node in update(), fixing issue (#5). It is done by adding "__block_replace = True" in the Settings node, either in the updated or updating Settings object.

--- qmworks/settings.py
+++ qmworks/settings.py
@@ -10,18 +10,27 @@ from six import string_types
 class Settings(plams.Settings, Storable):
     """
     This is a subclass of the :class:`plams.Settings`.
-    TODO: explain the differences between this class and plams' Settings
+    The difference with respect to plams' Settings are:
+    - settings['a.b'] is equivalent to settings['a']['b'] = settings.a.b
+    - in update(): settings.__block_replace = True results in removal of all existing key value pairs.
+      __block_replace can be either in the updated settings or in the updating settings object.
     """
 
     def __getitem__(self, name):
-        """Like regular __getitem__, but if name is a string, lowercase it."""
+        """
+        Like plams Settings.__getitem__, but
+        "settings['a.b'] == 'c'" is equivalent to "settings['a']['b'] == 'c'"
+        """
         if isinstance(name, string_types) and name.find('.') > -1:
             names = name.split('.')
             return dict.__getitem__(self, names[0]).__getitem__('.'.join(names[1:]))
         return dict.__getitem__(self, name)
 
     def __setitem__(self, name, value):
-        """Like regular __setitem__, but if name is a string, lowercase it."""
+        """
+        Like plams Settings.__setitem__, but
+        "settings['a.b'] = 'c'" is equivalent to "settings['a']['b'] = 'c'"
+        """
         if isinstance(value, dict):
             value = Settings(value)
         if isinstance(name, string_types) and name.find('.') > -1:
@@ -31,7 +40,10 @@ class Settings(plams.Settings, Storable):
             dict.__setitem__(self, name, value)
 
     def __delitem__(self, name):
-        """Like regular __delitem__, but if name is a string, lowercase it."""
+        """
+        Like plams Settings.__setitem__, but
+        "del settings['a.b']" is equivalent to "del settings['a']['b'] = 'c'"
+        """
         if isinstance(name, string_types) and name.find('.') > -1:
             names = name.split('.')
             dict.__getitem__(self, names[0]).__delitem__('.'.join(names[1:]))
@@ -39,8 +51,6 @@ class Settings(plams.Settings, Storable):
             dict.__delitem__(self, name)
 
     def copy(self):
-        """
-        """
         ret = Settings()
         for name in self:
             if isinstance(self[name], plams.Settings):
@@ -64,4 +74,41 @@ class Settings(plams.Settings, Storable):
         ret = self.copy()
         ret.update(other)
         return ret
+
+    def update(self, other):
+        """
+        Like PLAMS update, but:
+        __block_replace = True results in removing all existing key value pairs in the current node of the settings tree
+
+        For example:
+
+        >>> from qmworks import Settings
+        >>> t=Settings()
+        >>> t.input.xc.lda = ""
+        >>> u = Settings()
+        >>> u.input.xc.hybrid = 'b3lyp'
+        >>> print(t.overlay(u))
+        input:
+              xc:
+                 hybrid:     b3lyp
+                 lda:
+        >>> u.input.xc.__block_replace = True
+        >>> print(t.overlay(u))
+        input:
+              xc:
+                 __block_replace:     True
+                 hybrid:     b3lyp
+        """
+        for name in other:
+            if isinstance(other[name], Settings):
+                if name not in self or not isinstance(self[name], Settings):
+                    self[name] = other[name].copy()
+                else:
+                    # __block_replace can be used to remove all existing key value pairs in an input block
+                    if ('__block_replace' in other[name] and other[name]['__block_replace'] == True) or \
+                        ('__block_replace' in self[name] and self[name]['__block_replace'] == True):
+                        self[name] = Settings()
+                    self[name].update(other[name])
+            else:
+                self[name] = other[name]
 
