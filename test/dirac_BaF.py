8fc7f547fc9ab73c2e5ef11eb73937afd63adaa4
Felipe <felipe@bazis-h0.cm.cluster>
2016-08-09 18:16:00 +0200

    removed dead code

--- test/dirac_BaF.py
+++
@@ -1,7 +0,0 @@
-
-from qmworks import init, finish, Settings, templates
-from noodles import run_process
-from plams import Molecule
-
-# User Defined imports
-from qmworks.packages.SCM import adf, dftb
