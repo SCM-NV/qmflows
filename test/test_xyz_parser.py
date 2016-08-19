b86c16c8e93a2b13b8711d1c65666528a61adefa
felipe zapata <tifonzafel@gmail.com>
2016-08-19 17:35:30 +0200

    added a test for the xyz parser

---
+++ test/test_xyz_parser.py
@@ -0,0 +1,17 @@
+
+from qmworks.parsers import (manyXYZ, parse_string_xyz)
+from qmworks.utils import (chunksOf, flatten)
+
+
+def test_multiple_geometries():
+    """
+    Test the reading of multiples molecular geometries from a file.
+    """
+    path_xyz = 'test/test_files/five_points_ethylene.xyz'
+
+    with open(path_xyz, 'r') as f:
+        ls = f.readlines()
+
+    xs = [flatten(x) for x in chunksOf(ls, 8)]
+
+    assert list(map(parse_string_xyz, xs)) == manyXYZ(path_xyz)
