85656e6f3bced7bb22dbfa235d6a7368680dbf8f
felipe zapata <tifonzafel@gmail.com>
2016-09-30 12:00:53 +0200

    renamed freq_ethene to freq_ethene_DFTB (#24)

--- test/test_with_plams_init.py
+++ test/test_with_plams_init.py
@@ -2,7 +2,7 @@ import plams
 
 plams.init()
 
-exec(open('test/tests_using_plams/freq_ethene.py').read())
+exec(open('test/tests_using_plams/freq_ethene_DFTB.py').read())
 exec(open('test/tests_using_plams/ADFGeometry_Constraints.py').read())
 exec(open('test/tests_using_plams/mo_cp2k.py').read())
 exec(open('test/tests_using_plams/opt_orca.py').read())
