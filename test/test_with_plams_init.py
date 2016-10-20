f98fdd1ce40732c57f7c105006c49cfb78e922b4
felipe zapata <tifonzafel@gmail.com>
2016-10-20 11:11:32 +0200

    tests packages using a function (#37)

--- test/test_with_plams_init.py
+++ test/test_with_plams_init.py
@@ -1,30 +1,35 @@
+from nose.plugins.attrib import attr
 import importlib
 import plams
 
-"""
-``modules`` is a dictionary contains as key the python modules names and
-a list of functions as values.
-"""
-modules = {
-    'freq_ethene_DFTB': ['test_freq'],
-    'ADFGeometry_Constraints': ['test_ADFGeometry_Constraint'],
-    'mo_cp2k': ['test_ethylene'],
-    'opt_orca': ['test_opt_orca', 'test_methanol_opt_orca'],
-    'linear_TS': ['test_linear_ts'],
-    'get_properties': ['test_dftb_props', 'test_adf_props'],
-    'opt_gamess': ['test_opt_gamess']
-}
 
-plams.init()
+@attr('slow')
+def test_packages():
+    """
+    ``modules`` is a dictionary contains as key the python modules names and
+    a list of functions as values.
+    """
+    modules = {
+        'freq_ethene_DFTB': ['test_freq'],
+        'ADFGeometry_Constraints': ['test_ADFGeometry_Constraint'],
+        'mo_cp2k': ['test_ethylene'],
+        'opt_orca': ['test_opt_orca', 'test_methanol_opt_orca'],
+        'linear_TS': ['test_linear_ts'],
+        'get_properties': ['test_dftb_props', 'test_adf_props'],
+        'opt_gamess': ['test_opt_gamess']
+    }
 
-root_folder = 'test.tests_using_plams'
+    plams.init()
 
-# Load all the tests modules
-for module_name, fs in modules.items():
-    mod = root_folder + '.' + module_name
-    m = importlib.import_module(mod)
-    # Execute each test function inside the module
-    for fun in fs:
-        getattr(m, fun)()
+    root_folder = 'test.tests_using_plams'
 
-plams.finish()
+    # Load all the tests modules
+    for module_name, fs in modules.items():
+        print("testing modules: ", module_name)
+        mod = root_folder + '.' + module_name
+        m = importlib.import_module(mod)
+        # Execute each test function inside the module
+        for fun in fs:
+            getattr(m, fun)()
+
+    plams.finish()
