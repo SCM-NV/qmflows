903c500efac1e1a8c19ad767405f72dff2f85ffc
Lars Ridder <l.ridder@esciencecenter.nl>
2016-10-25 10:53:59 +0200

    Use exec_example function for FDE test (#40)

--- test/test_using_plams/test_FDE_Fragments.py
+++ test/test_using_plams/test_FDE_Fragments.py
@@ -16,15 +16,7 @@ def test_ADF3FDE_Cystine():
     """
     Test MFCC partitioning of cystine
     """
-    local_env = {}
-    global_env = {}
-    os.chdir('examples/FDE_Fragments')
-    try:
-        exec(open('ADF3FDE_Cystine.py').read(), global_env, local_env)
-        os.chdir('../..')
-    except:
-        os.chdir('../..')
-        raise RuntimeError()
+    local_env = exec_example('examples/FDE_Fragments', 'ADF3FDE_Cystine.py')
     for x,y in zip(local_env['supermol_dipole'], local_env['mfcc_dipole']):
         assert abs(x-y) < 0.01
 
@@ -33,15 +25,7 @@ def test_FDE_Fragments():
     """
     Test FDE Fragments
     """
-    local_env = {}
-    global_env = {}
-    os.chdir('examples/FDE_Fragments')
-    try:
-        exec(open('FDE_Fragments.py').read(), global_env, local_env)
-        os.chdir('../..')
-    except:
-        os.chdir('../..')
-        raise RuntimeError()
+    local_env = exec_example('examples/FDE_Fragments', 'FDE_Fragments.py')
     dipole_vect = local_env['dipole_fde']
     assert round(dipole_vect[0] - 0.71, 2) == 0
     assert round(dipole_vect[1], 2) == 0
