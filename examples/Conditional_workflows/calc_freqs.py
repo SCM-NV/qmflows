b180cc7db30e6a56321c0454bc2a923ba669eacc
Lars Ridder <l.ridder@esciencecenter.nl>
2016-12-21 20:23:08 +0100

    Annotate calc_freqs example (#40)

--- examples/Conditional_workflows/calc_freqs.py
+++ examples/Conditional_workflows/calc_freqs.py
@@ -1,32 +1,36 @@
 from noodles import gather
 from qmworks import dftb, adf, orca, run, Settings, templates, molkit, find_first_job
 
-# This examples illustrates that the possibility to use different packages
-# interchangeably. Analytical frequencies are available not for some functionals in ADF
-# This workflow captures the resulting error and submits the same job to ORCA
+# This examples illustrates the possibility to use different packages interchangeably.
+# Analytical frequencies are not available for B3LYP in ADF
+# This workflow captures the resulting error and submits the same job to ORCA.
 
-water = molkit.from_smiles('[OH2]')
-
-hartree_to_kcalpermol = 627.094
-
+# Define the condition for a successful calculation
 def is_successful(result):
     return result.status not in ["failed", "crashed"]
 
+# Generate water molecule
+water = molkit.from_smiles('[OH2]')
+
+# Pre-optimize the water molecule
 opt_water = dftb(templates.geometry, water, job_name="dftb_geometry")
 
 jobs = []
 
-# loop over functionals
+# Generate freq jobs for 3 functionals
 for functional in ['pbe', 'b3lyp', 'blyp']:
     s=Settings()
     s.basis = 'DZ'
     s.functional = functional
+    # Try to perform the jobs with adf or orca, take result from  first successful calculation
     freqjob = find_first_job(is_successful, [adf, orca], templates.freq.overlay(s),
                              opt_water.molecule, job_name=functional)
     jobs.append(freqjob)
 
+# Run workflow
 results = run(gather(*jobs), n_processes=1)
 
+# Print the result
 table = ""
 for i in range(3):
     table += "{:10s}{:10.3f}{:10.3f}{:10.3f}\n".\
