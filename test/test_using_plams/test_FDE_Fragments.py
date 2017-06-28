2a611e50c9b1b1206bd93f9f949c456c7ff3e468
Lars Ridder <l.ridder@esciencecenter.nl>
2017-06-28 11:16:23 +0200

    Update of ADF3FDE_Cystine test

--- test/test_using_plams/test_FDE_Fragments.py
+++ test/test_using_plams/test_FDE_Fragments.py
@@ -18,7 +18,9 @@ def test_ADF3FDE_Cystine():
     local_env = {}
     exec(open('examples/FDE_Fragments/ADF3FDE_Cystine.py').read(), {}, local_env)
     for x,y in zip(local_env['supermol_dipole'], local_env['mfcc_dipole']):
-        assert abs(x-y) < 0.01
+        assert abs(x-y) < 0.05
+    for x,y in zip(local_env['supermol_dipole'], local_env['adf3fde_dipole']):
+        assert abs(x-y) < 0.03
 
 @attr('slow')
 def test_FDE_Fragments():
