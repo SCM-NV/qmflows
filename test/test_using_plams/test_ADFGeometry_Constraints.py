7f8e2d081526fa8d1605c57d38ec81b9006fb006
Felipe Zapata <tifonzafel@gmail.com>
2017-06-28 11:32:56 +0200

    #81 testing ADF

--- test/test_using_plams/test_ADFGeometry_Constraints.py
+++ test/test_using_plams/test_ADFGeometry_Constraints.py
@@ -3,23 +3,23 @@ from qmworks.examples import (
     example_generic_constraints, example_H2O2_TS, example_partial_geometry_opt)
 import numpy as np
 
-# @attr('slow')
-# def test_partial_geometry_opt():
-#     """
-#     Test partial geometry optimization.
-#     """
-#     geom1, geom2 = example_partial_geometry_opt()
-#     assert str(geom1) == str(geom2)
+@attr('slow')
+def test_partial_geometry_opt():
+    """
+    Test partial geometry optimization.
+    """
+    geom1, geom2 = example_partial_geometry_opt()
+    assert str(geom1) == str(geom2)
 
 
-# @attr('slow')
-# def test_h2o2_ts():
-#     """
-#     Test TS optimization of a rotational barrier in hydrogen peroxide with ORCA using init hessian from DFTB
-#     """
-#     ts_dihe, n_optcycles = example_H2O2_TS()
-#     assert (n_optcycles < 7)
-#     assert ts_dihe == 0.0
+@attr('slow')
+def test_h2o2_ts():
+    """
+    Test TS optimization of a rotational barrier in hydrogen peroxide with ORCA using init hessian from DFTB
+    """
+    ts_dihe, n_optcycles = example_H2O2_TS()
+    assert (n_optcycles < 7)
+    assert ts_dihe == 0.0
 
 
 @attr('slow')
