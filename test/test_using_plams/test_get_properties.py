8b4bb1df61d35b06941510c8b74d5a18652a2984
Lars Ridder <l.ridder@esciencecenter.nl>
2017-06-28 11:18:50 +0200

    Make test_props slightly less picky on the frequency value

--- test/test_using_plams/test_get_properties.py
+++ test/test_using_plams/test_get_properties.py
@@ -14,7 +14,7 @@ def test_dftb_props():
     assert abs(result.energy - expected_energy) < 0.01
     assert len(result.dipole) == 3
     expected_frequency = 3460.92
-    assert abs(result.frequencies - expected_frequency) < 0.01
+    assert abs(result.frequencies - expected_frequency) < 0.1
     assert len(result.charges) == 2
     assert abs(result.charges[0] + result.charges[1]) < 1e-6
 
@@ -31,6 +31,6 @@ def test_adf_props():
     assert abs(result.energy - expected_energy) < 0.01
     assert len(result.dipole) == 3
     expected_frequency = 3480.90
-    assert abs(result.frequencies[1] - expected_frequency) < 0.01
+    assert abs(result.frequencies[1] - expected_frequency) < 0.1
     assert len(result.charges) == 2
     assert abs(result.charges[0] + result.charges[1]) < 1e-6
