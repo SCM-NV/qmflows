2f9c3751176b6982f189a2b2940ff16350d6fa9c
felipeZ <tifonzafel@gmail.com>
2018-01-29 10:47:58 +0100

    changed display output to Dumb

--- src/qmflows/packages/packages.py
+++ src/qmflows/packages/packages.py
@@ -22,7 +22,7 @@ import pkg_resources as pkg
 
 # ==================> Internal modules <====================
 from noodles import (schedule_hint, has_scheduled_methods, serial)
-from noodles.display import (NCDisplay)
+from noodles.display import (DumbDisplay, NCDisplay)
 from noodles.files.path import (Path, SerPath)
 from noodles.run.run_with_prov import run_parallel_opt
 from noodles.serial import (Serialiser, Registry, AsDict)
@@ -380,7 +380,7 @@ def call_default(job, n_processes=1, cache='cache.json'):
     """
     Run locally using several threads.
     """
-    with NCDisplay() as display:
+    with DumbDisplay() as display:
         return run_parallel_opt(
             job, n_threads=n_processes,
             registry=registry, jobdb_file=cache,
