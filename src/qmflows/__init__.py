282923194e6ddb5f66a1a2ad32c3ebea25080e60
felipe <tifonzafel@gmail.com>
2018-10-10 17:40:12 +0200

    removed dead code from utils

--- src/qmflows/__init__.py
+++ src/qmflows/__init__.py
@@ -6,7 +6,6 @@ from .packages import (
 from .parsers import *
 from .templates import *
 from .settings import *
-from .utils import *
 from .examples import (
     example_ADF3FDE_Cystine, example_ADF3FDE_Dialanine, example_FDE_fragments,
     example_H2O2_TS, example_freqs, example_generic_constraints, example_partial_geometry_opt)
