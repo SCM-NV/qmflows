cccb7e615011e3c176b0c8f23f36d2914b78909f
felipe <tifonzafel@gmail.com>
2018-10-11 17:12:32 +0200

    added test to parse cp2k output

---
+++ test/test_cp2k_parser.py
@@ -0,0 +1,13 @@
+from qmflows.parsers import cp2KParser
+import os
+
+
+def test_read_cp2k_coefficients():
+    """
+    """
+    path = "test/test_files/output_cp2k"
+    mo_file = os.path.join(path, "mo_coeffs.log")
+
+    data = cp2KParser.read_cp2k_coefficients(mo_file, path)
+
+    assert data.eigenVals.ndim == 1 and data.coeffs.ndim == 2
