b9fe4e6ffbf001e8e6ceffc9ac149f39d6745f8e
felipe <tifonzafel@gmail.com>
2018-10-15 18:32:18 +0200

    Added deepcopy method

--- src/qmflows/settings.py
+++ src/qmflows/settings.py
@@ -50,6 +50,9 @@ class Settings(plams.core.settings.Settings, ):
     def as_dict(self):
         return dict(self)
 
+    def __deepcopy__(self, _):
+        return self.copy()
+
     @classmethod
     def from_dict(cls, **kwargs):
         return cls(**kwargs)
