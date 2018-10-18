bb4c07b160f99a3e4b944aa6f16b2f1bae27bd39
felipe <tifonzafel@gmail.com>
2018-10-18 16:10:37 +0200

    removed PES reactivity

--- src/qmflows/components/__init__.py
+++ src/qmflows/components/__init__.py
@@ -1,7 +1,7 @@
 from .fde import (ADF3FDE_Result, Fragment, MFCC_Result, adf3fde, adf_fragmentsjob, mfcc)
 from .operations import (find_first_job, select_max, select_min)
-from .reactivity import (Distance, Angle, Dihedral, PES)
+from .reactivity import (Distance, Angle, Dihedral)
 
 __all__ = [
     'ADF3FDE_Result', 'Fragment', 'MFCC_Result', 'adf3fde', 'adf_fragmentsjob', 'mfcc',
-    'find_first_job', 'select_max', 'select_min', 'Distance', 'Angle', 'Dihedral', 'PES']
+    'find_first_job', 'select_max', 'select_min', 'Distance', 'Angle', 'Dihedral']
--- src/qmflows/components/reactivity.py
+++ src/qmflows/components/reactivity.py
@@ -1,9 +1,7 @@
 
-__all__ = ['Distance', 'Angle', 'Dihedral', 'PES']
+__all__ = ['Distance', 'Angle', 'Dihedral']
 
-from qmflows import templates
 from qmflows.settings import Settings
-from noodles import gather, schedule
 from scm.plams import Molecule
 from rdkit.Chem import AllChem
 import scm.plams.interfaces.molecule.rdkit as molkit
@@ -75,95 +73,3 @@ class Dihedral(Coordinate):
     def get_current_value(self, mol, rad=False):
         self.fun = AllChem.GetDihedralRad if rad else AllChem.GetDihedralDeg
         return super().get_current_value(mol)
-
-
-@schedule
-class PES:
-    def __init__(
-            self, molecule=None, constraints=None, offset=None,
-            get_current_values=False,
-            nsteps=0, stepsize=0.0, nested_PES=None):
-
-        self.molecule = molkit.to_rdmol(molecule)
-        self.constraints = constraints
-        if isinstance(constraints, list):
-            self.start = []
-            for i, cs in enumerate(constraints):
-                if offset is None:
-                    self.start.append(0.0)
-                else:
-                    self.start.append(offset[i])
-                if get_current_values:
-                    self.start[i] += cs.get_current_value(self.molecule)
-        else:
-            if offset is None:
-                self.start = 0.0
-            else:
-                self.start = offset
-            if get_current_values:
-                self.start += constraints.get_current_value(self.molecule)
-        self.nsteps = nsteps
-        self.stepsize = stepsize
-        self.nested_PES = nested_PES
-
-    def scan(self, package, settings, job_name="PESscan"):
-        """
-        This function enables multidimensional potential energy surface scans.
-        The argument 'scan' should be a dictionary with keys 'constraint' and 'surface'
-        and optionally 'scan'. The latter allows nested (multidimensional) scans.
-
-        Example
-        .. code-block:: python
-
-            scan = {'constraint': "dist 1 5",
-                    'surface': {'nsteps':6, 'start': 2.3, 'stepsize': 0.1},
-                    'scan': {'constraint': "dist 3 4",
-                             'surface': {'nsteps':6, 'start': 2.3, 'stepsize': 0.1}
-                             }
-                   }
-
-        It is also possible to scan over two coordinates in a concerted way, for example:
-        .. code-block:: python
-
-            scan = {'constraint': ["dist 1 5", "dist 3 4"],
-                       'surface': {'nsteps':6, 'start':[2.3,2.3], 'stepsize': [0.1,0.1]} }
-
-        In this example multiple listed values for 'start' and 'stepsize' correspond to
-        the same number of listed 'constraint's
-
-        :returns:
-            A list of promised result objects
-        """
-        pes_jobs = []
-
-        for step in range(self.nsteps):
-            new_settings = settings.overlay(self.get_constraint_settings(step))
-            name = job_name + "_" + str(step)
-            if self.nested_PES:
-                pes_jobs.append(self.nested_PES.scan(package, new_settings, name))
-            else:
-                pes_jobs.append(self.pes_job(package, new_settings, name))
-        return gather(*pes_jobs)
-
-    def pes_job(self, package, settings, job_name):
-        if isinstance(package, list):
-            name = job_name + "_opt"
-            optimized_mol = package[0](templates.geometry.overlay(settings),
-                                       self.molecule, job_name=name).molecule
-            result = package[1](templates.singlepoint.overlay(settings),
-                                optimized_mol, job_name=job_name)
-        else:
-            result = package(templates.geometry.overlay(settings), self.molecule,
-                             job_name=job_name)
-        return result
-
-    def get_constraint_settings(self, step):
-        s = Settings()
-        if isinstance(self.constraints, list):
-            for c in range(len(self.constraints)):
-                s.constraint.update(
-                    self.constraints[c].get_settings(self.start[c] +
-                                                     self.stepsize[c] * step))
-        else:
-            s.constraint = self.constraints.get_settings(self.start + self.stepsize * step)
-        return s
