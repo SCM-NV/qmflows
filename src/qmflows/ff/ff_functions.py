f5026ec423ea7305cf6f7ac3c9f43ea75ccc7ff6
Bas van Beek <b.f.van.beek@vu.nl>
2019-01-31 11:27:06 +0100

    Update ff_functions.py

--- src/qmflows/ff/ff_functions.py
+++ src/qmflows/ff/ff_functions.py
@@ -10,19 +10,10 @@ from scm.plams.core.basemol import Molecule
 from qmflows.components.qd_functions import to_array
 
 
-path = '/Users/bvanbeek/Documents/CdSe/Week_5/QD'
-mol = 'Core_Cd68Se55__26_Ligand_OC[CCCCCF]CCCCCCCCCC@O1.pdb'
-mol = Molecule(join(path, mol))
-mol.properties.name = 'QD'
-
-# Create xyz arrays with Cd, Se, O
-Cd_array = to_array([at for at in mol if at.symbol == 'Cd'])
-Se_array = to_array([at for at in mol if at.symbol == 'Se'])
-O_array = to_array([at for at in mol if at.symbol == 'O'])
-
-
 def get_radial_distr(array1, array2, dr=0.01, r_max=10.0):
     """ Calculate the radial distribution function between *array1* and *array2*.
+    array1 <np.ndarray>: A n*3 array representing the cartesian coordinates of the reference atoms.
+    array2 <np.ndarray>: A m*3 array representing the cartesian coordinates.
     dr <float>: The integration step-size in Angstrom.
     r_max <float>: The maximum to be returned interatomic distance.
     """
@@ -30,7 +21,8 @@ def get_radial_distr(array1, array2, dr=0.01, r_max=10.0):
     dist_int = np.array(dist / dr, dtype=np.int64).flatten()
 
     r = np.arange(dr, r_max + dr, dr)
-    dens_mean = (len(array2) / (4 * np.pi * (dist.max() / 2)**3))
+    dens_mean = len(array2) / ((4/3) * np.pi * (0.5 * dist.max())**3)
+
     dens = np.bincount(dist_int)[1:1+int(r_max/dr)]
     dens = dens / len(array1)
     dens /= (4 * np.pi * r**2 * dr)
@@ -38,9 +30,19 @@ def get_radial_distr(array1, array2, dr=0.01, r_max=10.0):
 
     return dens
 
-# Plot that shit
-dr, r_max = 0.01, 10.0
-r = np.arange(dr, r_max + dr, dr)
-dens = get_radial_distr(Cd_array, Cd_array, dr=dr, r_max=r_max)
-plt.plot(r, dens, 'r')
 
+path = '/Users/basvanbeek/Documents/CdSe/Week_5/QD'
+mol = 'Core_Cd360Se309__102_Ligand_CCCCCCCCCCCCCCCO@O16.pdb'
+mol = Molecule(join(path, mol))
+mol.properties.name = 'QD'
+
+# Create xyz arrays with Cd, Se, O
+Cd_array = to_array([at for at in mol if at.symbol == 'Cd'])[None, :, :]
+Se_array = to_array([at for at in mol if at.symbol == 'Se'])[None, :, :]
+O_array = to_array([at for at in mol if at.symbol == 'O'])[None, :, :]
+
+# Plot it
+dr, r_max = 0.02, 10.0
+r = np.arange(dr, r_max + dr, dr)
+dens = np.array([get_radial_distr(i, j, dr=dr) for i, j in zip(Cd_array, O_array)]).mean(axis=0)
+plt.plot(r, dens, 'r')
