b70d5d071f1021381a6fbf638114e57cd2bc77d6
felipez <tifonzafel@gmail.com>
2019-11-27 14:35:30 +0100

    removed Dirac interface

--- src/qmflows/packages/__init__.py
+++ src/qmflows/packages/__init__.py
@@ -6,8 +6,7 @@ from .cp2k_package import cp2k
 from .SCM import (adf, dftb)
 from .orca import orca
 from .gamess import gamess
-from .dirac import dirac
 
 __all__ = ['Package', 'Result', 'SerMolecule', 'SerSettings',
-           'adf', 'cp2k', 'dftb', 'dirac', 'gamess', 'orca',
+           'adf', 'cp2k', 'dftb', 'gamess', 'orca',
            'package_properties', 'run', 'registry']
