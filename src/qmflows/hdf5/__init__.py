875322e93f132d52ee2c5da040664ca7b8d63dd5
felipez <tifonzafel@gmail.com>
2019-11-27 13:59:15 +0100

    removed dump_to_hdf5 function
    Signed-off-by: felipez <tifonzafel@gmail.com>

--- src/qmflows/hdf5/__init__.py
+++ src/qmflows/hdf5/__init__.py
@@ -1,4 +1,3 @@
-from .hdf5_functions import dump_to_hdf5
 from .quantumHDF5 import (StoreasHDF5, cp2k2hdf5)
 
-__all__ = ['StoreasHDF5', 'cp2k2hdf5', 'dump_to_hdf5']
+__all__ = ['StoreasHDF5', 'cp2k2hdf5']
