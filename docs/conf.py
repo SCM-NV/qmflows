494c97d93edfbe7383215f25a9be81b606123e53
felipez <tifonzafel@gmail.com>
2020-03-06 11:25:08 +0100

    sphinx mock rdkit and h5py #151

--- docs/conf.py
+++ docs/conf.py
@@ -202,6 +202,13 @@ intersphinx_mapping = {
 # New in version 2.1.
 autodoc_typehints = 'none'
 
+# This value contains a list of modules to be mocked up.
+# This is useful when some external dependencies are not met at build time and break the building process.
+# You may only specify the root package of the dependencies themselves and omit the sub-modules:
+autodoc_mock_imports = [
+    'rdkit',
+    'h5py',
+]
 
 rst_epilog = """
 .. |Package| replace:: :class:`~qmflows.packages.Package`
