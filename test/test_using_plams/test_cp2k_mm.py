31687698b84a0874ab5a5c4951ca2ac281e8deec
Bas van Beek <b.f.van.beek@vu.nl>
2020-03-17 14:29:34 +0100

    Simplified tests

--- test/test_using_plams/test_cp2k_mm.py
+++ test/test_using_plams/test_cp2k_mm.py
@@ -1,16 +1,9 @@
-from io import TextIOBase
-from itertools import islice
-from typing import Union, Any
-from collections import abc
-
 import numpy as np
 import pytest
 from assertionlib import assertion
-from scm.plams import Molecule, add_to_instance, add_to_class, Units, Cp2kJob
+from scm.plams import Molecule
 
 from qmflows import Settings, run, cp2k_mm, singlepoint, geometry, freq, md
-from qmflows.type_hints import PathLike
-from qmflows.backports import nullcontext
 from qmflows.test_utils import delete_output, get_mm_settings, PATH, PATH_MOLECULES
 
 MOL = Molecule(PATH_MOLECULES / 'Cd68Cl26Se55__26_acetate.xyz')
@@ -37,11 +30,13 @@ def overlap_coords(xyz1: np.ndarray, xyz2: np.ndarray) -> np.ndarray:
     return xyz1 @ rotmat.T
 
 
-def get_energy(self, index: int = -1, unit: str = 'Hartree') -> float:
-    """Return the energy of the last occurence of ``'ENERGY| Total FORCE_EVAL'`` in the output."""
-    energy_str = self.grep_output('ENERGY| Total FORCE_EVAL')[index]
-    energy = float(energy_str.rsplit(maxsplit=1)[1])
-    return Units.convert(energy, 'Hartree', unit)
+from scm.plams import Cp2kJob, add_to_class
+
+
+@add_to_class(Cp2kJob)
+def get_runscript(self):
+    inp, out = self._filename('inp'), self._filename('out')
+    return f'cp2k.ssmp -i {inp} -o {out}'
 
 
 @pytest.mark.slow
@@ -55,15 +50,9 @@ def test_singlepoint() -> None:
     result = run(job, path=PATH)
     assertion.eq(result.status, 'successful')
 
-    # Yes, this is a small hack as neither energy nor get_energy() seems to work
-    plams_results = result.results
-    add_to_instance(plams_results)(get_energy)
-    setattr(result, 'get_energy', plams_results.get_energy)
-
     # Compare energies
     ref = -15.4431781758
-    energy = result.get_energy()
-    assertion.isclose(energy, ref, rel_tol=10**-4)
+    assertion.isclose(result.energy, ref, rel_tol=10**-4)
 
 
 @pytest.mark.slow
@@ -77,19 +66,13 @@ def test_geometry() -> None:
     result = run(job, path=PATH)
     assertion.eq(result.status, 'successful')
 
-    # Yes, this is a small hack as neither energy nor get_energy() seems to work
-    plams_results = result.results
-    add_to_instance(plams_results)(get_energy)
-    setattr(result, 'get_energy', plams_results.get_energy)
-
     # Compare energies
     ref = -16.865587192150834
-    energy = result.get_energy()
-    assertion.isclose(energy, ref, rel_tol=10**-4)
+    assertion.isclose(result.energy, ref, rel_tol=10**-4)
 
     # Compare geometries
     xyz_ref = np.load(PATH_MOLECULES / 'Cd68Cl26Se55__26_acetate.npy')
-    _xyz = Molecule(plams_results['cp2k-pos-1.xyz'], geometry=500).as_array()
+    _xyz = Molecule(result.results['cp2k-pos-1.xyz'], geometry=500).as_array()
     _xyz -= _xyz.mean(axis=0)[None, ...]
     xyz = overlap_coords(_xyz, xyz_ref)
     np.testing.assert_allclose(xyz, xyz_ref, rtol=np.inf, atol=0.05)
@@ -108,9 +91,7 @@ def test_freq() -> None:
     result = run(job, path=PATH)
     assertion.eq(result.status, 'successful')
 
-    plams_results = result.results
-
-    freqs = get_frequencies(plams_results['cp2k-VIBRATIONS-1.mol'])
+    freqs = result.frequencies
     freqs_ref = np.load(PATH / 'Cd68Cl26Se55__26_acetate.freq.npy')
     np.testing.assert_allclose(freqs, freqs_ref, rtol=np.inf, atol=5.0)
 
