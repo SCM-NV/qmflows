6f1859ea244fdcc705599faa604249284aaf35a4
felipez <tifonzafel@gmail.com>
2020-03-22 17:04:01 +0100

    added xyz missing test for parser #169

--- test/test_xyz_parser.py
+++ test/test_xyz_parser.py
@@ -2,7 +2,7 @@
 from assertionlib import assertion
 from more_itertools import chunked
 
-from qmflows.parsers import manyXYZ, parse_string_xyz
+from qmflows.parsers import manyXYZ, parse_string_xyz, readXYZ
 from qmflows.test_utils import PATH, PATH_MOLECULES
 
 
@@ -21,3 +21,7 @@ def test_multiple_geometries():
 def test_xyz_reader():
     """Test the xyz parser from a file."""
     path_qd = PATH_MOLECULES / "Cd68Cl26Se55__26_acetate.xyz"
+    mol = readXYZ(path_qd)
+    for atom in mol:
+        assertion.le(len(atom.symbol), 2)
+        assertion.len_eq(atom.xyz, 3)
