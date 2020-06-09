49a74a062bda85508433dfbc0401e64a56099db5
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2020-06-09 15:20:38 +0200

    Use `InitRestart` when running QMFlows jobs (#198)
    * Exposed ``InitRestart`` to the main QMFlows ``__init__.py`` file.
    * Exchanged ``plams.init()`` / ``plams.finish()`` for ``qmflows.InitRestart`` in the ``qmflows.run()`` function.
    * Store the ``cache.db`` file in the PLAMS working directory.

--- src/qmflows/__init__.py
+++ src/qmflows/__init__.py
@@ -4,6 +4,8 @@ from .__version__ import __version__
 
 from .logger import logger
 
+from .utils import InitRestart
+
 from .components import (
     Angle, Dihedral, Distance,
     find_first_job, select_max, select_min)
@@ -19,6 +21,7 @@ from .examples import (example_H2O2_TS, example_freqs, example_generic_constrain
 __all__ = [
     '__version__',
     'logger',
+    'InitRestart',
     'Angle', 'Dihedral', 'Distance', 'Settings',
     'adf', 'cp2k', 'cp2k_mm', 'dftb', 'orca', 'run', 'PackageWrapper',
     'example_H2O2_TS', 'example_freqs', 'example_generic_constraints',
