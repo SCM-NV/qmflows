ca4a5394473b2d3d646aaa325bac2ec3010dbb06
Bas van Beek <b.f.van.beek@vu.nl>
2020-12-21 12:31:55 +0100

    ENH: Add a template for cell optimizations

--- src/qmflows/__init__.py
+++ src/qmflows/__init__.py
@@ -13,7 +13,7 @@ from .components import (
 from .packages import (
     adf, cp2k, cp2k_mm, dftb, orca, run, PackageWrapper)
 
-from .templates import (freq, geometry, singlepoint, ts, md)
+from .templates import (freq, geometry, singlepoint, ts, md, cell_opt)
 from .settings import Settings
 from .examples import (example_H2O2_TS, example_freqs, example_generic_constraints,
                        example_partial_geometry_opt)
@@ -25,5 +25,6 @@ __all__ = [
     'Angle', 'Dihedral', 'Distance', 'Settings',
     'adf', 'cp2k', 'cp2k_mm', 'dftb', 'orca', 'run', 'PackageWrapper',
     'example_H2O2_TS', 'example_freqs', 'example_generic_constraints',
-    'example_partial_geometry_opt', 'freq', 'geometry', 'singlepoint', 'ts', 'md',
+    'example_partial_geometry_opt',
+    'freq', 'geometry', 'singlepoint', 'ts', 'md', 'cell_opt',
     'find_first_job', 'select_max', 'select_min']
