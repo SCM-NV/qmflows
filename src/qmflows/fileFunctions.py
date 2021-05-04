69bdb22c277326c599117dbac90d9e7300a80f08
Bas van Beek <b.f.van.beek@vu.nl>
2021-05-04 17:37:43 +0200

    ENH: Replace `yaml.FullLoader` with `UniqueSafeLoader`

--- src/qmflows/fileFunctions.py
+++ src/qmflows/fileFunctions.py
@@ -2,18 +2,22 @@
 
 __all__ = ['yaml2Settings']
 
-from typing import AnyStr, Callable
+from typing import Union, Callable
 
 import yaml
 
 from .settings import Settings
 from .type_hints import T
+from .yaml_utils import UniqueSafeLoader
 
 
-def yaml2Settings(xs: AnyStr, mapping_type: Callable[[dict], T] = Settings) -> T:
+def yaml2Settings(
+    xs: Union[str, bytes],
+    mapping_type: Callable[[dict], T] = Settings,
+) -> T:
     """Transform a string containing some data in .yaml format to a Settings object."""
     if isinstance(xs, bytes):
         xs = xs.decode()
 
-    dct = yaml.load(xs, Loader=yaml.FullLoader)  # yaml object must be string
+    dct = yaml.load(xs, Loader=UniqueSafeLoader)  # yaml object must be string
     return mapping_type(dct)
