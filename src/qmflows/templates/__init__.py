84f47293205012644b9b1c2d9eeb9632a89deb9d
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2022-04-06 17:07:15 +0200

    MAINT, DEP: Restructure the `qmflows.templates` sub-package
    Restructure the sub-package into the following public namespaces:
    * `templates`

--- src/qmflows/templates/__init__.py
+++ src/qmflows/templates/__init__.py
@@ -1,5 +1,5 @@
 """Various Settings templates used throughout QMFlows."""
 
-from .templates import (freq, geometry, singlepoint, ts, md, cell_opt)
+from ._templates import (freq, geometry, singlepoint, ts, md, cell_opt)
 
 __all__ = ['freq', 'geometry', 'singlepoint', 'ts', 'md', 'cell_opt']
